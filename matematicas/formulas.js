75e44de0e11ed7f0557c45a1aca96cb0fd5a0fc2
victorcuervo <vcuervo@gmail.com>
2015-06-15 00:20:51 +0200

    Cambio en Formulas Matemáticas de javascript

--- matematicas/formulas.js
+++ matematicas/formulas.js
@@ -11,19 +11,9 @@ function formulas() {
 		return Math.PI * Math.pow(radio,2);
 	}
 	
-	this.hipotenusa = function (c1,c2) {
-		return Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2));
-		// Aunque esta como experimental se poría poner return Math.hypot(cateto1,cateto2);
-	}
-	
+	/* Número Aleatorio */
 	this.aleatorio = function (maximo) {
 		return parseInt(Math.random()*maximo+1);
-	}
-	
-	// Como sobrescribirlo para pasarle un array
-	this.aleatorio2 = function (array) {
-		return array[this.aleatorio(array.length-1)];
-		
 	}
 
 }
@@ -35,10 +25,4 @@ console.log ("El área de un círculo de radio 2 es " + f.areaCirculo(2));
 for (x=0;x<10;x++) {
 	// Números aleatorios entre 1 y 10
 	console.log (f.aleatorio(10));
-}
-
-var a = [80,81,82,83,84,85];
-for (x=0;x<10;x++) {
-	// Números aleatorios entre 1 y 10
-	console.log (f.aleatorio2(a));
 }

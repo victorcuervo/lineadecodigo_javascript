d65c054f9ec0812d92262c63eef66eac83cb4fe3
Carmen Rodrigo <carmen.rodrigo@gmail.com>
2015-08-02 23:08:56 +0200

    Funciones factorial de un número

--- matematicas/factorial.js
+++ matematicas/factorial.js
@@ -1,5 +1,25 @@
 /**
- * 
+ * Dos funciones para calcular el factorial de un número entero
  */
 
-console.log ("mi factorial en js"); 
+function factorialRecursivo (n) { 
+	if (n == 0){ 
+		return 1; 
+	}
+	return n * factorialRecursivo (n-1); 
+}
+
+function factorial (n) {
+	var total = 1; 
+	for (i=1; i<=n; i++) {
+		total = total * i; 
+	}
+	return total; 
+}
+
+
+console.log ("Calcula el factorial javascript"); 
+
+console.log("El factorial de 5 con bucle: "+ factorial (5));
+
+console.log("El factorial de 5 recursivo: "+ factorialRecursivo (5));

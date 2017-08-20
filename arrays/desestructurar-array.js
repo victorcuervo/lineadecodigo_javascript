a0f1a16356f3004204ab79285e7e496ce7c2d466
Víctor Cuervo <vcuervo@gmail.com>
2017-08-21 00:10:42 +0200

    Desestructurar un array en Javascript

---
+++ arrays/desestructurar-array.js
@@ -0,0 +1,20 @@
+/**
+ * @file deconstruir-array.js
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   20/agosto/2017
+ * @url  http://lineadecodigo.com/typescript/xxx/
+ * @description Realizar una deconstrucción de un array
+*/
+
+var miarray = [1,2,3,4,5];
+var [a,b] = miarray;
+
+console.log(a);
+console.log(b);
+
+var [x,y, ...resto] = miarray;
+
+console.log(x);
+console.log(y);
+console.log(resto);

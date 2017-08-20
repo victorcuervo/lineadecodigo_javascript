ec254382a927a0c4bdb880d0f1d8ead957e64c2b
Víctor Cuervo <vcuervo@gmail.com>
2017-08-21 00:25:55 +0200

    Recorrer un array en Javascript

---
+++ arrays/recorrer-array.js
@@ -0,0 +1,13 @@
+/**
+ * @file recorrer-array.js
+ * @version 2.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   21/agosto/2017
+ * @url  http://lineadecodigo.com/javascript/recorrer-un-array-en-javascript/
+ * @description Recorrer los elementos de un array en Javascript
+*/
+
+var a=[1,2,3,4,5,6,7,8,9,10];
+for (x=0;x<a.length;x++){
+	console.log(a[x]);
+}

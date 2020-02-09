aa007344cd43c926d84a06ff05715aaae9a72190
Víctor Cuervo <vcuervo@gmail.com>
2020-02-09 20:31:38 +0100

    Fechas relativas en formato texto utilizando Locales.

---
+++ fechas/fechas-relativas-texto.js
@@ -0,0 +1,23 @@
+/**
+ * @file fechas-relativas-texto.js
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   09/febrero/2020
+ * @url  http://lineadecodigo.com/javascript/pdte/
+ * @description Utilizar formatos relativos para poner en texto fechas.s
+*/
+
+// Si eliminas la propiedad numeric lo muestra en números.
+const rtf1 = new Intl.RelativeTimeFormat('es-ES', { style: 'long', numeric:'auto' });
+
+// Dentro de tres trimestres
+console.log(rtf1.format(3, 'quarter'));
+
+// El mes pasado
+console.log(rtf1.format(-1, 'month'));
+
+// Ayer
+console.log(rtf1.format(-1, 'day'));
+
+// Pasado mañana
+console.log(rtf1.format(2, 'day'));

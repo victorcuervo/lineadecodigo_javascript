6b3415fac7f5548bd3f083f271e8f4aa832f115f
Víctor Cuervo <vcuervo@gmail.com>
2020-09-13 01:56:46 +0200

    Dígitos significantes

---
+++ i18n/digitos-significantes.js
@@ -0,0 +1,19 @@
+/**
+ * @file digitos-significantes.js
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   23/septiembre/2020
+ * @url  http://lineadecodigo.com/javascript/pdte/
+ * @description Formatear números para dejar solo los dígito significantes.
+*/
+
+const cantidades = [1235467.64,24535467.12,3456783.2,15653231,8976562,3122122];
+
+cantidades.forEach(element => {
+    console.log(new Intl.NumberFormat('es-ES', { maximumSignificantDigits: 2 }).format(element));
+});
+
+
+
+
+

48acc2720c0825d300650bcc094e47622d5d409a
Víctor Cuervo <vcuervo@gmail.com>
2020-09-13 01:51:35 +0200

    Formatear monedas

---
+++ i18n/formatear-monedas.js
@@ -0,0 +1,16 @@
+/**
+ * @file formatear-monedas.js
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   23/septiembre/2020
+ * @url  http://lineadecodigo.com/javascript/pdte/
+ * @description Ejemplo que nos explica cómo dar formato a las monedas
+*/
+
+
+const cantidad = 1235.64;
+
+console.log(new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'EUR' }).format(cantidad));
+console.log(new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(cantidad));
+console.log(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(cantidad));
+

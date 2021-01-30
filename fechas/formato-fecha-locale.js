8fc58079b88292bbeac5db1192369fada02497de
victorcuervo <vcuervo@gmail.com>
2021-01-30 20:47:52 +0100

    Includo artículo

--- fechas/formato-fecha-locale.js
+++ fechas/formato-fecha-locale.js
@@ -3,22 +3,22 @@
  * @version 1.0
  * @author Víctor Cuervo - http://lineadecodigo.com
  * @date   15/marzo/2020
- * @url  http://lineadecodigo.com/javascript/pdte/
+ * @url  http://lineadecodigo.com/javascript/fechas-en-formato-local-con-javascript/
  * @description Mostrar una fecha en formato de texto atendiendo a un locale concreto.
 */
 
-const event = new Date(Date.UTC(2020, 02, 15, 12, 0, 0));
+const fecha = new Date(Date.UTC(2020, 02, 15, 12, 0, 0));
 
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 // Castellano
-console.log(event.toLocaleDateString('es-ES', options));
+console.log(fecha.toLocaleDateString('es-ES', options));
 
 // Alemán
-console.log(event.toLocaleDateString('de-DE', options));
+console.log(fecha.toLocaleDateString('de-DE', options));
 
 // Árabe
-console.log(event.toLocaleDateString('ar-EG', options));
+console.log(fecha.toLocaleDateString('ar-EG', options));
 
 // Por defecto del sistema
-console.log(event.toLocaleDateString(undefined, options));
+console.log(fecha.toLocaleDateString(undefined, options));

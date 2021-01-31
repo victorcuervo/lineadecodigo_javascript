8328a6a07e8e5709c6cd0efa00df195b65d1101e
victorcuervo <vcuervo@gmail.com>
2021-01-31 15:31:45 +0100

    Números Primos del 1 al 100

---
+++ matematicas/esPrimo.js
@@ -0,0 +1,21 @@
+/**
+ * @file esPrimo.js
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   31/enero/2021
+ * @url   http://lineadecodigo.com/javascript/pdte/
+ * @description  Cálculo de los números primos del 1 al 100
+ */
+
+
+function esPrimo(numero) {
+    
+    for(let i = 2,raiz=Math.sqrt(numero); i <= raiz; i++)
+        if(numero % i === 0) return false;
+    return numero > 1;
+}  
+
+for (let x=0;x<=100;x++) {
+    if (esPrimo(x))
+        console.log("El número " + x + " es primo");
+}

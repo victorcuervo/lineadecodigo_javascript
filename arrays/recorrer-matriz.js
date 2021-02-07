344cb3d29d34f67181c3f0e7e89cf9aa955263b5
victorcuervo <vcuervo@gmail.com>
2021-02-07 01:11:57 +0100

    Recorrer matriz en Javascript

---
+++ arrays/recorrer-matriz.js
@@ -0,0 +1,18 @@
+/**
+ * @file recorrer-matriz.js
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   7/febrero/2021
+ * @url  http://lineadecodigo.com/javascript/pdte/
+ * @description Recorrer los elementos de una matriz en Javascript
+*/
+
+const matriz = [[1,2,3],[4,5,6],[7,8,9]];
+
+for (x=0;x<matriz.length;x++) {
+    text = ""
+    for (y=0;y<matriz[x].length;y++) {
+        text += matriz[x][y] + "\t";
+    }
+    console.log(text)
+}

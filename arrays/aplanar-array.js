7c7ee25609cc33631ec75aa715f7f1b14c0c40d4
victorcuervo <vcuervo@gmail.com>
2021-02-12 11:31:06 +0100

    Subido artículo

--- arrays/aplanar-array.js
+++ arrays/aplanar-array.js
@@ -3,17 +3,17 @@
  * @version 1.0
  * @author Víctor Cuervo - http://lineadecodigo.com
  * @date   7/febrero/2021
- * @url  http://lineadecodigo.com/javascript/xxx/
+ * @url  http://lineadecodigo.com/javascript/aplanar-arrays-en-javascript/
  * @description Código que nos permite aplanar los elementos de un array
 */
 
-const a=[[1,2],[3,4,5,6],[7],[8,9,10]];
+const a=[[1,2],[3,[4,5],6],[7],[8,9,10]];
 
 // Recorrer el array multidimensional
 for (x=0;x<a.length;x++)
 	for (y=0;y<a[x].length;y++) 
 		console.log(a[x][y]);
 
-const a_flat = a.flat();
+const a_flat = a.flat(2);
 for (x=0;x<a_flat.length;x++)
 	console.log(a_flat[x]);

fb1e3b7c551e32cf52acc63c5d835cc1d77ad3ed
victorcuervo <vcuervo@gmail.com>
2021-02-14 00:50:26 +0100

    Rellenar un array de números aleatorios

---
+++ arrays/rellenar-array-numeros-aleatorios.js
@@ -0,0 +1,15 @@
+/**
+ * @file rellenar-array-numeros-aleatorios.js
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   13/febrero/2021
+ * @url  http://lineadecodigo.com/javascript/pdte/
+ * @description Rellena los elementos de un array con números aleatorios
+*/
+
+
+let miarray = Array.from({length: 10}, () => Math.floor(Math.random() * 10));
+
+miarray.forEach(function(item,index,arr){
+    console.log(item);
+});

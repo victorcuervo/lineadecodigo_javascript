e302bb2eab44718fdac39e043ba7c0e329f70f66
victorcuervo <vcuervo@gmail.com>
2021-02-14 00:47:42 +0100

    Rellenar un Array en Javascript

---
+++ arrays/rellenar-array.js
@@ -0,0 +1,16 @@
+/**
+ * @file rellenar-array.js
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   13/febrero/2021
+ * @url  http://lineadecodigo.com/javascript/pdte/
+ * @description Rellena los elementos de un array con un valor mediante un método fill.
+*/
+
+
+let miarray = new Array(10);
+miarray.fill(2,0,10);
+
+miarray.forEach(function(item,index,arr){
+    console.log(item);
+});

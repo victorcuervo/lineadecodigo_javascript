c46e6be08e5cb80d11953ef7dfaea3af9b85e581
victorcuervo <vcuervo@gmail.com>
2021-02-14 02:14:17 +0100

    Variable global en Javascript

---
+++ basicos/variable-global.js
@@ -0,0 +1,31 @@
+/**
+ * @file variable-global.js
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   14/febrero/2021
+ * @url  http://lineadecodigo.com/javascript/pdte/
+ * @description Variables globales en Javascript
+*/
+
+
+// Variables Globales
+var n1 = 2;
+var n2 = 3;
+var suma = 0;
+
+function sumar() {
+    suma = n1+n2;
+}
+
+function imprimir() {
+    console.log(suma);
+}
+
+imprimir();
+sumar();
+imprimir();
+
+
+
+
+

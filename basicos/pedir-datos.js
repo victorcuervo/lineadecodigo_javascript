1ebb116d1d62b79e6008608fc54082e6b767ee19
victorcuervo <vcuervo@gmail.com>
2021-02-14 01:02:01 +0100

    pedir datos mediante un prompt

---
+++ basicos/pedir-datos.js
@@ -0,0 +1,13 @@
+/**
+ * @file pedir-dayos.js
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   27/agosto/2017
+ * @url  http://lineadecodigo.com/javascript/pdte/
+ * @description Pedir un dato mediante un prompt
+*/
+
+let valor1 = prompt("Dame el valor 1");
+let valor2 = prompt("Dame el valor 2");
+
+console.log("La suma de " + valor1 + "+" + valor2 + " es " + valor1+valor2);

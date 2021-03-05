f55b8f8f21a411d671e94285182dd835bea68985
victorcuervo <vcuervo@gmail.com>
2021-03-05 23:56:27 +0100

    Artículo publicado

--- basicos/pedir-datos.js
+++ basicos/pedir-datos.js
@@ -3,7 +3,7 @@
  * @version 1.0
  * @author Víctor Cuervo - http://lineadecodigo.com
  * @date   13/febrero/2021
- * @url  http://lineadecodigo.com/javascript/pdte/
+ * @url  http://lineadecodigo.com/javascript/pedir-datos-con-prompt/
  * @description Pedir un dato mediante un prompt
 */
 

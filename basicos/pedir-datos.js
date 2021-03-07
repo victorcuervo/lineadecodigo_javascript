7e1acbe24222e47c9dd6d3fc9d40068b4f66d1fe
victorcuervo <vcuervo@gmail.com>
2021-03-08 00:48:17 +0100

    Añadido typeod y control de vacio

--- basicos/pedir-datos.js
+++ basicos/pedir-datos.js
@@ -10,6 +10,11 @@
 let valor1 = prompt("Dame el valor 1",0);
 let valor2 = prompt("Dame el valor 2",0);
 
+console.log("El contenido de un prompt es del tipo: " + typeof(valor1));
+
+valor1 = (valor1=="")?"0":valor1;
+valor2 = (valor2=="")?"0":valor2;
+
 let suma = parseInt(valor1) + parseInt(valor2); 
 
 console.log("La suma de " + valor1 + "+" + valor2 + " es " + suma);

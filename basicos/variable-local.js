1a2bba422df230e8f4c048e70f6b9e5dd8c91676
victorcuervo <vcuervo@gmail.com>
2021-09-26 18:16:04 +0200

    Añadidos URLs

--- basicos/variable-local.js
+++ basicos/variable-local.js
@@ -3,7 +3,7 @@
  * @version 1.0
  * @author Víctor Cuervo - http://lineadecodigo.com
  * @date   14/febrero/2021
- * @url  http://lineadecodigo.com/javascript/pdte/
+ * @url  https://lineadecodigo.com/javascript/variables-locales-en-javascript/
  * @description Variables locales en Javascript
 */
 

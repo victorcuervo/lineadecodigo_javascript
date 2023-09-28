cec9f311d9f4344e8f2cb09bdb0b37801049d8c6
victorcuervo <vcuervo@gmail.com>
2023-09-28 21:31:06 +0200

    documentacion math.max

--- numeros/mayor-de-dos-numeros-math.js
+++ numeros/mayor-de-dos-numeros-math.js
@@ -3,14 +3,20 @@
  * @version 1.0
  * @author Víctor Cuervo - http://lineadecodigo.com
  * @date   28/mayo/2023
- * @url  http://lineadecodigo.com/javascript/pdte/
+ * @url  http://lineadecodigo.com/javascript/mayor-de-dos-numeros-en-javascript-con-math/
  * @description Saber cual de los dos números es mayor con el objeto Math
 */
-
-let numeros = [5,2,4,7,14];
-
 
 let n1 = 5;
 let n2 = 2;
 
+// Con estructuras
+if (n1>n2)
+  console.log("El mayor de los números " + n1 + " y " + n2 + " es " + n1);
+else if (n2>n1)
+  console.log("El mayor de los números " + n1 + " y " + n2 + " es " + n2);
+else
+  console.log("Los números " + n1 + " y " + n2 + " son iguales");
+
+  // Con Math
 console.log("El mayor de los números " + n1 + " y " + n2 + " es " + Math.max(n1,n2));
